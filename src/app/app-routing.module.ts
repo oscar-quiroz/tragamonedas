edef3e09fa7bcc9901021debf12dde4342ee7641
oscar-quiroz <kairox.rt@gmail.com>
2021-09-26 11:55:42 -0500

    pagina principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { SimulacionComponent } from './pages/simulacion/simulacion.component';
+import { EstadisticasComponent } from './pages/estadisticas/estadisticas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: InicioComponent
+  },
+  {
+    path:'Simulacion', component: SimulacionComponent
+  },
+  {
+    path:'Estadisticas', component: EstadisticasComponent
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { SimulacionComponent } from './pages/simulacion/simulacion.component';
+import { EstadisticasComponent } from './pages/estadisticas/estadisticas.component';
+import { MaquinaComponent } from './components/maquina/maquina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    SimulacionComponent,
+    EstadisticasComponent,
+    MaquinaComponent
   ],
   imports: [
     BrowserModule,
