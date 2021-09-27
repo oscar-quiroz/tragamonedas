a8d987b9d8cb9db8d700d215a687e81224e0c835
oscar-quiroz <kairox.rt@gmail.com>
2021-09-27 16:41:20 -0500

    iniciar simulacion

--- src/app/pages/simulacion/simulacion.component.ts
+++ src/app/pages/simulacion/simulacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-simulacion',
@@ -10,6 +11,10 @@ export class SimulacionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  iniciar(){
+
   }
 
 }
