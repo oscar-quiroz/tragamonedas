ff1e1c8946d9cde6254537ec770285339060b24a
EstebanG <esteban.gutierrez@uptc.edu.co>
2021-10-03 17:48:19 -0500

    Stats agregado

--- src/app/pages/simulacion/simulacion.component.ts
+++ src/app/pages/simulacion/simulacion.component.ts
@@ -11,7 +11,7 @@ export class SimulacionComponent implements OnInit {
 
   constructor(private simulationService:SlotMachineServiceService) { }
 
-  public data=[];
+  public data:any=[];
 
   public starter= 'animate__fadeInLeft';
 
@@ -26,15 +26,203 @@ export class SimulacionComponent implements OnInit {
   iniciar(){
     this.load=true;
     this.starter='animate__fadeOutLeftBig';
-    setTimeout(() => {
-      this.data = this.simulationService.play();
+
+    /*this.data = [
+      {
+          "name": "Game 0",
+          "player": {
+              "id": 0,
+              "name": "Player 0",
+              "startMoney": 1000000,
+              "earnedMoney": 0,
+              "lostMoney": 1000000,
+              "finalMoney": 0,
+              "timePlayed": 482,
+              "winRounds": 190,
+              "lostRounds": 292
+          },
+          "machine": {
+              "id": 0,
+              "name": "SlotMachine 0"
+          }
+      },
+      {
+          "name": "Game 1",
+          "player": {
+              "id": 1,
+              "name": "Player 1",
+              "startMoney": 1000000,
+              "earnedMoney": 3144000,
+              "lostMoney": 0,
+              "finalMoney": 4144000,
+              "timePlayed": 720,
+              "winRounds": 304,
+              "lostRounds": 416
+          },
+          "machine": {
+              "id": 1,
+              "name": "SlotMachine 1"
+          }
+      },
+      {
+          "name": "Game 2",
+          "player": {
+              "id": 2,
+              "name": "Player 2",
+              "startMoney": 1000000,
+              "earnedMoney": 0,
+              "lostMoney": 1000000,
+              "finalMoney": 0,
+              "timePlayed": 541,
+              "winRounds": 215,
+              "lostRounds": 326
+          },
+          "machine": {
+              "id": 2,
+              "name": "SlotMachine 2"
+          }
+      },
+      {
+          "name": "Game 3",
+          "player": {
+              "id": 3,
+              "name": "Player 3",
+              "startMoney": 1000000,
+              "earnedMoney": 0,
+              "lostMoney": 1000000,
+              "finalMoney": 0,
+              "timePlayed": 630,
+              "winRounds": 280,
+              "lostRounds": 350
+          },
+          "machine": {
+              "id": 3,
+              "name": "SlotMachine 3"
+          }
+      },
+      {
+          "name": "Game 4",
+          "player": {
+              "id": 4,
+              "name": "Player 4",
+              "startMoney": 1000000,
+              "earnedMoney": 0,
+              "lostMoney": 1000000,
+              "finalMoney": 0,
+              "timePlayed": 375,
+              "winRounds": 161,
+              "lostRounds": 214
+          },
+          "machine": {
+              "id": 4,
+              "name": "SlotMachine 4"
+          }
+      },
+      {
+          "name": "Game 5",
+          "player": {
+              "id": 5,
+              "name": "Player 5",
+              "startMoney": 1000000,
+              "earnedMoney": 998000,
+              "lostMoney": 0,
+              "finalMoney": 1998000,
+              "timePlayed": 720,
+              "winRounds": 284,
+              "lostRounds": 436
+          },
+          "machine": {
+              "id": 5,
+              "name": "SlotMachine 5"
+          }
+      },
+      {
+          "name": "Game 6",
+          "player": {
+              "id": 6,
+              "name": "Player 6",
+              "startMoney": 1000000,
+              "earnedMoney": 0,
+              "lostMoney": 961000,
+              "finalMoney": 39000,
+              "timePlayed": 720,
+              "winRounds": 288,
+              "lostRounds": 432
+          },
+          "machine": {
+              "id": 6,
+              "name": "SlotMachine 6"
+          }
+      },
+      {
+          "name": "Game 7",
+          "player": {
+              "id": 7,
+              "name": "Player 7",
+              "startMoney": 1000000,
+              "earnedMoney": 0,
+              "lostMoney": 1000000,
+              "finalMoney": 0,
+              "timePlayed": 413,
+              "winRounds": 165,
+              "lostRounds": 248
+          },
+          "machine": {
+              "id": 7,
+              "name": "SlotMachine 7"
+          }
+      },
+      {
+          "name": "Game 8",
+          "player": {
+              "id": 8,
+              "name": "Player 8",
+              "startMoney": 1000000,
+              "earnedMoney": 0,
+              "lostMoney": 1000000,
+              "finalMoney": 0,
+              "timePlayed": 452,
+              "winRounds": 186,
+              "lostRounds": 266
+          },
+          "machine": {
+              "id": 8,
+              "name": "SlotMachine 8"
+          }
+      },
+      {
+          "name": "Game 9",
+          "player": {
+              "id": 9,
+              "name": "Player 9",
+              "startMoney": 1000000,
+              "earnedMoney": 0,
+              "lostMoney": 1000000,
+              "finalMoney": 0,
+              "timePlayed": 538,
+              "winRounds": 224,
+              "lostRounds": 314
+          },
+          "machine": {
+              "id": 9,
+              "name": "SlotMachine 9"
+          }
+      }
+  ];*/
     
+    setTimeout(() => {
+      this.simulationService.play().subscribe((data: any) => {
+        console.log(data);
+
+        this.data = data;})
+    }, 1000)
+
+    setTimeout(() => {
       if(this.data.length > 0 ){
         this.isData=true;
         this.load=false;
       }
-      
-    }, 1000);
+    }, 4000);
    
   }
 
--- src/app/slot-machine-service.service.ts
+++ src/app/slot-machine-service.service.ts
@@ -16,194 +16,67 @@ export class SlotMachineServiceService {
 
   play() {
     //MEtodo para llamar el back
-    /*this.http.get<any>("http://localhost:8082/api/casino/play").subscribe((data: any) => {
+   /* this.http.get<any>("http://localhost:8082/api/casino/play").subscribe((data: any) => {
             console.log(data);
-        });
-      }*/
 
-     
-      //Este Data es el archivo identico en json que llega al front
+            return data;
+        });
+      }*/ 
+      
+  return this.http.get<any>("http://localhost:8082/api/casino/play");
+      }
+
+  getStats(){
+    //return this.http.get<any>("http://localhost:8082/api/casino/stats");
     return [
       {
-        "name": "Game 0",
-        "player": {
-          "id": 0,
-          "name": "Player 0",
-          "startMoney": 1000000,
-          "earnedMoney": 0,
-          "lostMoney": 1000000,
-          "finalMoney": 0,
-          "timePlayed": 601,
-          "winRounds": 254,
-          "lostRounds": 347
-        },
-        "machine": {
-          "id": 0,
-          "name": "SlotMachine 0"
-        }
+          "name": "3 BAR",
+          "count": 0
       },
       {
-        "name": "Game 1",
-        "player": {
-          "id": 1,
-          "name": "Player 1",
-          "startMoney": 1000000,
-          "earnedMoney": 0,
-          "lostMoney": 1000000,
-          "finalMoney": 0,
-          "timePlayed": 579,
-          "winRounds": 240,
-          "lostRounds": 339
-        },
-        "machine": {
-          "id": 1,
-          "name": "SlotMachine 1"
-        }
+          "name": "3 Seven",
+          "count": 3
       },
       {
-        "name": "Game 2",
-        "player": {
-          "id": 2,
-          "name": "Player 2",
-          "startMoney": 1000000,
-          "earnedMoney": 0,
-          "lostMoney": 568000,
-          "finalMoney": 432000,
-          "timePlayed": 720,
-          "winRounds": 286,
-          "lostRounds": 434
-        },
-        "machine": {
-          "id": 2,
-          "name": "SlotMachine 2"
-        }
+          "name": "3 Cherries",
+          "count": 15
       },
       {
-        "name": "Game 3",
-        "player": {
-          "id": 3,
-          "name": "Player 3",
-          "startMoney": 1000000,
-          "earnedMoney": 0,
-          "lostMoney": 71000,
-          "finalMoney": 929000,
-          "timePlayed": 720,
-          "winRounds": 312,
-          "lostRounds": 408
-        },
-        "machine": {
-          "id": 3,
-          "name": "SlotMachine 3"
-        }
+          "name": "3 de cualquier simbolo",
+          "count": 257
       },
       {
-        "name": "Game 4",
-        "player": {
-          "id": 4,
-          "name": "Player 4",
-          "startMoney": 1000000,
-          "earnedMoney": 2653000,
-          "lostMoney": 0,
-          "finalMoney": 3653000,
-          "timePlayed": 720,
-          "winRounds": 293,
-          "lostRounds": 427
-        },
-        "machine": {
-          "id": 4,
-          "name": "SlotMachine 4"
-        }
+          "name": "Cualquiera y 2 Cherries",
+          "count": 262
       },
       {
-        "name": "Game 5",
-        "player": {
-          "id": 5,
-          "name": "Player 5",
-          "startMoney": 1000000,
-          "earnedMoney": 0,
-          "lostMoney": 1000000,
-          "finalMoney": 0,
-          "timePlayed": 375,
-          "winRounds": 151,
-          "lostRounds": 224
-        },
-        "machine": {
-          "id": 5,
-          "name": "SlotMachine 5"
-        }
+          "name": "2 Cualquiera y 1 Cherrie",
+          "count": 1760
       },
       {
-        "name": "Game 6",
-        "player": {
-          "id": 6,
-          "name": "Player 6",
-          "startMoney": 1000000,
-          "earnedMoney": 0,
-          "lostMoney": 1000000,
-          "finalMoney": 0,
-          "timePlayed": 540,
-          "winRounds": 241,
-          "lostRounds": 299
-        },
-        "machine": {
-          "id": 6,
-          "name": "SlotMachine 6"
-        }
+          "name": "3 diferentes",
+          "count": 3294
       },
       {
-        "name": "Game 7",
-        "player": {
-          "id": 7,
-          "name": "Player 7",
-          "startMoney": 1000000,
-          "earnedMoney": 0,
-          "lostMoney": 1000000,
-          "finalMoney": 0,
-          "timePlayed": 438,
-          "winRounds": 182,
-          "lostRounds": 256
-        },
-        "machine": {
-          "id": 7,
-          "name": "SlotMachine 7"
-        }
+          "name": "Promedio tiempo de juego",
+          "count": 559
       },
       {
-        "name": "Game 8",
-        "player": {
-          "id": 8,
-          "name": "Player 8",
-          "startMoney": 1000000,
-          "earnedMoney": 0,
-          "lostMoney": 1000000,
-          "finalMoney": 0,
-          "timePlayed": 407,
-          "winRounds": 171,
-          "lostRounds": 236
-        },
-        "machine": {
-          "id": 8,
-          "name": "SlotMachine 8"
-        }
+          "name": "Dinero ganado por el casino",
+          "count": 3819000
       },
       {
-        "name": "Game 9",
-        "player": {
-          "id": 9,
-          "name": "Player 9",
-          "startMoney": 1000000,
-          "earnedMoney": 0,
-          "lostMoney": 1000000,
-          "finalMoney": 0,
-          "timePlayed": 635,
-          "winRounds": 264,
-          "lostRounds": 371
-        },
-        "machine": {
-          "id": 9,
-          "name": "SlotMachine 9"
-        }
+          "name": "Dinero ganado por los jugadores",
+          "count": 6181000
+      },
+      {
+          "name": "Dinero total invertido por los jugadores",
+          "count": 10000000
+      },
+      {
+          "name": "Total rondas jugadas",
+          "count": 5591
       }
-    ];
+  ]
   }
 }
