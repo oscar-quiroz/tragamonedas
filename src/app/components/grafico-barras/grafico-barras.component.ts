df106efc8968071dc3889b6ff6df59be0f1f7ef6
oscar-quiroz <kairox.rt@gmail.com>
2021-10-04 11:25:21 -0500

    pagina de estadsiticas

--- src/app/components/grafico-barras/grafico-barras.component.ts
+++ src/app/components/grafico-barras/grafico-barras.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
-import { Label } from 'ng2-charts';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafico-barras',
@@ -10,7 +10,6 @@ import { Label } from 'ng2-charts';
 export class GraficoBarrasComponent implements OnInit {
   public barChartOptions: ChartOptions = {
     responsive: true,
-    // We use these empty structures as placeholders for dynamic theming.
     scales: { xAxes: [{}], yAxes: [{}] },
     plugins: {
       datalabels: {
@@ -19,26 +18,31 @@ export class GraficoBarrasComponent implements OnInit {
       },
     },
   };
-  public barChartLabels: Label[] = [
-    '2006',
-    '2007',
-    '2008',
-    '2009',
-    '2010',
-    '2011',
-    '2012',
+
+  @Input() public colors: Color[] = [
+    { backgroundColor: ['#6857e6', '#009fee', '#f02059'] },
   ];
+
+  @Input() public barChartLabels: Label[] 
+
+
   public barChartType: ChartType = 'bar';
+
   public barChartLegend = true;
 
-  public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-  ];
+
+  
+  @Input() public barChartData: ChartDataSets[];
+
+
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  
+  }
+
+
 
   // events
   public chartClicked({
@@ -61,16 +65,5 @@ export class GraficoBarrasComponent implements OnInit {
     console.log(event, active);
   }
 
-  public randomize(): void {
-    // Only Change 3 values
-    this.barChartData[0].data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      Math.random() * 100,
-      56,
-      Math.random() * 100,
-      40,
-    ];
-  }
+
 }
--- src/app/pages/estadisticas/estadisticas.component.ts
+++ src/app/pages/estadisticas/estadisticas.component.ts
@@ -1,15 +1,86 @@
 import { Component, OnInit } from '@angular/core';
+import { SlotMachineServiceService } from '../../slot-machine-service.service';
+import { Label } from 'ng2-charts';
+import { ChartDataSets } from 'chart.js';
 
 @Component({
   selector: 'app-estadisticas',
   templateUrl: './estadisticas.component.html',
-  styleUrls: ['./estadisticas.component.css']
+  styleUrls: ['./estadisticas.component.css'],
 })
 export class EstadisticasComponent implements OnInit {
+  public starter = 'animate__fadeInLeft';
+  public isData = false;
+  public data: any = [];
+  public load = false;
+  public labels: string[] = [];
+  public barData: number[] = [];
 
-  constructor() { }
+  public totalInvertido:number;
+  public totalGanadoCasino:number;
+  public totalGanadoJugadores:number;
+  public porcentajeGanancia:number;
 
-  ngOnInit(): void {
+  public promedioT:number;
+  public RondasT:number;
+
+  public chartData: ChartDataSets[] = [
+    { data: this.barData, label: 'Estadisticas', backgroundColor: '#0e8ad17c',hoverBackgroundColor: '#0e8ad1' },
+  ];
+
+  constructor(private simulationService: SlotMachineServiceService) {}
+
+  ngOnInit(): void {}
+
+  iniciar() {
+   
+if(!this.isData){
+  
+  this.load = true;
+  this.starter = 'animate__fadeOutLeftBig';
+  this.data = this.simulationService.getStats();
+
+
+  setTimeout(() => {
+    this.data.forEach((element) => {
+      this.labels.push(element.name);
+      this.barData.push(element.count);
+      this.labels.splice(7, 11);
+      this.barData.splice(7, 11);
+
+
+
+  this.isData = true;
+  this.load=false;
+    });
+    this.porcentajeGanancia= this.calcularPorcentaje();
+    this.obtenerTiempo();
+  }, 1000);
+
+
+  
+
+
+  
+}
+
   }
 
+  obtenerTiempo(){
+    this.promedioT= this.data[7].count ;
+  console.log("promedio de timepo: ", this.promedioT)
+    this.RondasT= this.data[11].count;
+    console.log("Rondas jugadas: ", this.RondasT)
+
+  }
+
+
+  calcularPorcentaje(){
+    this.totalGanadoCasino= this.data[8].count;
+  console.log("ganado casino: ", this.totalGanadoCasino)
+    this.totalInvertido= this.data[10].count;
+    console.log("invertido: ", this.totalInvertido)
+    return (this.totalGanadoCasino*100) / this.totalInvertido
+
+  }
 }
